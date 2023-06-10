a10482e310fe4fc537e9c24bcd7eb0508c7fa159
Azeem-Khan1 <aalookid@gmail.com>
2023-06-09 19:05:49 -0700

    Some more progress

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+
+// Copying the links to clipboard (On click of link icon)
+
+const shareButtons = document.querySelectorAll(".link-icon");
+
+async function copyText(e) {
+    e.preventDefault();
+    const link = this.getAttribute('link');
+    console.log(link);
+    try {
+        await navigator.clipboard.writeText(link);
+        alert("Copied the link: " + link);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+shareButtons.forEach((shareButton) =>
+    shareButton.addEventListener("click", copyText)
+);
